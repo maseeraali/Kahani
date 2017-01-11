f72fde12ca0cc76b7006a128fac9b3dc9dad2eb4
maseeraali <maseeraali28@gmail.com>
2017-01-11 23:30:52 +0000

    basic version

---
+++ app/src/main/java/com/example/maseera/kahani/MovieDetailActivity.java
@@ -0,0 +1,93 @@
+package com.example.maseera.kahani;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.DateFormatSymbols;
+
+public class MovieDetailActivity extends AppCompatActivity {
+    private MovieDetail mMovieInfo = new MovieDetail("title", "url", "url", "synopsis", "date", 5);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Bundle intent = getIntent().getExtras();
+        if (intent != null) {
+            mMovieInfo = intent.getParcelable("EXTRA_TEXT");
+        }
+        getSupportActionBar().setTitle(mMovieInfo.getTitle());
+        // If we don't get useable backdrop value, show a colored background matching the primary
+        // app color.
+        ImageView mBackDropImageView = (ImageView)findViewById(R.id.detail_backdrop_image_view);
+        ImageView mPosterImageView = (ImageView)findViewById(R.id.detail_poster_image_view);
+        TextView mRatingTextView = (TextView)findViewById(R.id.detail_rating_text_view);
+        RatingBar mRatingBar = (RatingBar) findViewById(R.id.detail_rating_bar);
+        TextView mReleaseDateTextView = (TextView) findViewById(R.id.detail_release_date_text_view);
+        TextView mSynopsisTextView = (TextView) findViewById(R.id.detail_synopsis_text_view);
+        String backdropImgUrl = mMovieInfo.getBackdropImgUrl();
+        if (backdropImgUrl.equals("null")) {
+            mBackDropImageView.setImageResource(R.drawable.placeholder_null);
+        } else {
+            Picasso.with(this)
+                    .load(backdropImgUrl)
+                    .placeholder(R.drawable.placeholder_backdrop)
+                    .into(mBackDropImageView);
+        }
+
+        Picasso.with(this)
+                .load(mMovieInfo.getPosterImgUrl())
+                .placeholder(R.drawable.placeholder_poster)
+                .into(mPosterImageView);
+
+        // Avoid showing floats when the value is a whole number ie. 7.0 / 10
+        double rating = mMovieInfo.getVoteAvg();
+        if (rating == (int) rating) {
+            mRatingTextView.setText((int) rating + " / 10");
+        } else {
+            mRatingTextView.setText(rating + " / 10");
+        }
+
+        mRatingBar.setRating((float) mMovieInfo.getVoteAvg()/2);
+        mReleaseDateTextView.setText(parseDateString(mMovieInfo.getReleaseDate()));
+        mSynopsisTextView.setText(mMovieInfo.getSynopsis());
+
+    }
+
+    public String parseDateString(String date) {
+        String[] parts = date.split("-");
+        int year = Integer.valueOf(parts[0]);
+        int month = Integer.valueOf(parts[1]);
+        int day = Integer.valueOf(parts[2]);
+
+        return new DateFormatSymbols().getMonths()[month - 1] + " " + day + ", " + year;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        } else if(id == android.R.id.home) {
+            super.onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
